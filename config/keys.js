825f4a1119ec668ed1c5fec17c327de32d8c3c88
Salman Hilabi <alamdardevelopment@gmail.com>
2020-01-17 14:20:13 +0530

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,7 @@
 module.exports = {
   secretOrKey: "secret",
   emailPassword:
-    "eH&Zxasru1awSH6Ru95Nxy9Zbkupqct@48PFoC0tYuW41F8Sw9wSH5*%MJTh^61&ntF8L4c&B4%Z7c17#wkcTb9f5%A^DtVDE2&3",
+    "your-email-password",
   mongoURL:
-    "mongodb+srv://mern-to-do:mern-to-do@messages-oddkg.mongodb.net/test?retryWrites=true&w=majority"
+    "your-mongo-uri"
 };
